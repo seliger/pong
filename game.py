507a0f872aac9c482010765d8e5fe65704044189
Corey S. Seliger <corey@seligerfamily.org>
2021-05-17 12:55:48 -0400

    Changed framerate to 60

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import sys
 import random
-import pygame   
+import pygame
 
 from pygame.locals import *
 from pygame.rect import *
@@ -11,11 +11,11 @@ GREEN = (0, 255, 0)
 GRAY = (180, 180, 180)
 
 BALL_SIZE = 20
-BALL_SPEED = 3
+BALL_SPEED = 12
 
 PADDLE_HEIGHT = 100
 PADDLE_WIDTH = 20
-PADDLE_SPEED = 6
+PADDLE_SPEED = 24
 
 BORDERWIDTH = 25
 
@@ -32,7 +32,7 @@ mid_screen = width / 2, height / 2
 # Instantiate our game board objects
 screen = pygame.display.set_mode(screen_size)
 fence = Rect(0, 0, width, height)
-#midfield = 
+#midfield =
 paddles = [
     Rect(int((width * 0.15)), int(mid_screen[1] - (PADDLE_HEIGHT / 2)), PADDLE_WIDTH, PADDLE_HEIGHT),
     Rect(int((width * 0.85)), int(mid_screen[1] - (PADDLE_HEIGHT / 2)), PADDLE_WIDTH, PADDLE_HEIGHT)
@@ -83,7 +83,7 @@ while game_loop:
 
 
     # Parse through any events that have occurred sice the last loop
-    for event in pygame.event.get():            
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_loop = False
 
@@ -129,25 +129,25 @@ while game_loop:
                 speed[1] = -BALL_SPEED
             # If we hit the bottom part of the paddle, ensure the ball bounces down
             elif (ball.top - paddles[collision].top) + (BALL_SIZE / 2) > round(PADDLE_HEIGHT * 0.55):
-                speed[1] = BALL_SPEED 
+                speed[1] = BALL_SPEED
             # If we hit the midsection, the ball bounces in a straight line
             else:
                 speed[1] = 0
-            
+
 
     # Place all the objects for the current frame
     screen.fill(BLACK)
     pygame.draw.rect(screen, GRAY, fence, BORDERWIDTH * 2)
     for paddle in paddles:
         pygame.draw.rect(screen, GRAY, paddle)
-        
+
     pygame.draw.rect(screen, GRAY, ball)
 
-    # Display the new frame 
+    # Display the new frame
     pygame.display.update()
 
     # Ensure the game runs at a reasonable speed
-    game_clock.tick(240)
+    game_clock.tick(60)
 
 
 # Shutdown pygame gracefully
