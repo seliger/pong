a7f3a93d5845dbb85cf22dac61982c977a0bfcd7
Corey S. Seliger <corey@seligerfamily.org>
2021-05-20 18:48:07 -0400

    Bugfixes, edits for Repl.it
    * Fixed fence; replaced with routine to draw 4 separate rectangles
    * Changed code to start working with repl.it online python/pygame
      display
      * Switched back to full screen (does not work on Chromebook)
      * Determine screen size (different on repl.it)
      * Reduced size of things since repl.it runs in a smaller resolution

--- game.py
+++ game.py
@@ -10,34 +10,47 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 GRAY = (180, 180, 180)
 
-BALL_SIZE = 20
+BALL_SIZE = 15
 BALL_SPEED = 12
 
-PADDLE_HEIGHT = 100
-PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 80
+PADDLE_WIDTH = 10
 PADDLE_SPEED = 24
 
-BORDERWIDTH = 25
+BORDERWIDTH = 10
+
 
 # Initialize pygame
 pygame.init()
 
-# Accept repeat keys
-pygame.key.set_repeat(1,10)
 
 # Compute geometry of screen
-screen_size = width, height = 1600, 900
+screen_info = pygame.display.Info()
+screen_size = width, height = (screen_info.current_w, screen_info.current_h)
 mid_screen = width / 2, height / 2
 
 # Instantiate our game board objects
-screen = pygame.display.set_mode(screen_size)
-fence = Rect(0, 0, width, height)
+screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
 #midfield =
 paddles = [
-    Rect(int((width * 0.15)), int(mid_screen[1] - (PADDLE_HEIGHT / 2)), PADDLE_WIDTH, PADDLE_HEIGHT),
-    Rect(int((width * 0.85)), int(mid_screen[1] - (PADDLE_HEIGHT / 2)), PADDLE_WIDTH, PADDLE_HEIGHT)
+    Rect(int((width * 0.10)), int(mid_screen[1] - (PADDLE_HEIGHT / 2)), PADDLE_WIDTH, PADDLE_HEIGHT),
+    Rect(int((width * 0.90)), int(mid_screen[1] - (PADDLE_HEIGHT / 2)), PADDLE_WIDTH, PADDLE_HEIGHT)
     ]
 ball = Rect(mid_screen[0], mid_screen[1], BALL_SIZE, BALL_SIZE)
+
+
+pygame.display.set_caption("Pong")
+
+# Accept repeat keys
+pygame.key.set_repeat(1,10)
+
+
+def draw_borders(s, x, y, w, h, bw, c):
+    pygame.draw.rect(s, c, (x, y, w, bw))
+    pygame.draw.rect(s, c, (x, y+h-bw, w, bw))
+    pygame.draw.rect(s, c, (x, y, bw, h))
+    pygame.draw.rect(s, c, (x+w-bw, y, bw, h))
+
 
 # Randomly start the ball in one direction or the other for either axis.
 # Speed is a list of two values [x, y] that drives the horizontal and
@@ -72,15 +85,22 @@ while game_loop:
     if ball.left < (width * 0.28) and speed[0] < 0:
         if ball.bottom < paddles[0].top and paddles[0].top > BORDERWIDTH:
             paddles[0].move_ip([0, -PADDLE_SPEED])
+
         elif ball.top > paddles[0].bottom and paddles[1].bottom < (height - BORDERWIDTH):
             paddles[0].move_ip([0, PADDLE_SPEED])
 
-    if ball.left > (width * 0.72) and speed[0] > 0:
+    if ball.right > (width * 0.72) and speed[0] > 0:
         if ball.bottom < paddles[1].top and paddles[0].top > BORDERWIDTH:
             paddles[1].move_ip([0, -PADDLE_SPEED])
         elif ball.top > paddles[1].bottom and paddles[1].bottom < (height - BORDERWIDTH):
             paddles[1].move_ip([0, PADDLE_SPEED])
 
+    # Check the paddles to make sure they don't exceed the border walls
+    for paddle in paddles:
+        if paddle.top < BORDERWIDTH:
+            paddle.move_ip([0, BORDERWIDTH - paddle.top])
+        elif paddle.bottom > height - BORDERWIDTH:
+            paddle.move_ip([0, -(BORDERWIDTH - (height - paddle.bottom))])
 
     # Parse through any events that have occurred sice the last loop
     for event in pygame.event.get():
@@ -90,8 +110,16 @@ while game_loop:
     # Advance the ball forward, based on previously set speed parameters.
     ball.move_ip(speed)
 
-    # If the ball touches the top or bottom border, bounce
-    if ball.top < BORDERWIDTH or ball.bottom > height - BORDERWIDTH:
+    # # If the ball touches the top or bottom border, bounce
+    # if ball.top < BORDERWIDTH or ball.bottom > height - BORDERWIDTH:
+    #     speed[1] = -speed[1]
+
+    # Check to ensure the ball is within the fence, else adjust accordingly
+    if ball.top < BORDERWIDTH:
+        ball.move_ip([0, BORDERWIDTH - ball.top])
+        speed[1] = -speed[1]
+    elif ball.bottom > height - BORDERWIDTH:
+        ball.move_ip([0, -(BORDERWIDTH - (height - ball.bottom))])
         speed[1] = -speed[1]
 
     # If the ball ends up behind a paddle, reset the ball and record the score
@@ -137,7 +165,9 @@ while game_loop:
 
     # Place all the objects for the current frame
     screen.fill(BLACK)
-    pygame.draw.rect(screen, GRAY, fence, BORDERWIDTH * 2)
+
+    draw_borders(screen, 0, 0, width, height, BORDERWIDTH, GRAY)
+
     for paddle in paddles:
         pygame.draw.rect(screen, GRAY, paddle)
 
